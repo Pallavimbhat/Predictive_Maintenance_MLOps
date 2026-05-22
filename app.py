6ab6ff6ed5974f1a0d1db04d0f30001c02cb4d69
Pallavimbhat <pallavimb24082005@gmail.com>
2026-05-22 21:16:07 +0530

    reupdated app.y

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -29,5 +30,8 @@ def predict():
         "prediction": labels[int(prediction)]
     })
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
